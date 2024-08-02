3faa538a43527d1fa2c58de5c8d00f6ed6f48fdc
mohan <mohan14242@gmail.com>
2024-08-02 13:42:02 +0530

    mohan

--- 4.sh
+++ 4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-if [ "$EUID" -eq 0]; then 
+if [ "$EUID" -eq 0 ]; then 
     echo "the $0 running under the root user" 
 else
     echo "the script is not running under the root user" 
