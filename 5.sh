203902c80e4d6891314b4ba4a3b2cc3d57a1ece0
mohan <mohan14242@gmail.com>
2024-08-02 13:46:57 +0530

    mohan

--- 5.sh
+++ 5.sh
@@ -2,4 +2,9 @@
 ##creting the log file with name of the svript containing 
 
 log_file="$0.log"
-echo "the script executed at $(date)" >> $log_file
+if [ "$EUID" -eq 0 ]; then 
+    echo "the script executed at $(date) by the root user" >> $log_file
+else 
+    echo "the script executed at $(date) by normal user" >> $log_file
+    
+fi
