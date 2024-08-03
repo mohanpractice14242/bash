ca3cf125dc09d5c47fd8126a4f3ab459db1e62a4
mohan <mohan14242@gmail.com>
2024-08-03 19:20:43 +0530

    mohan

---
+++ arrays/3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+###herer we are adding the elements to the empty array 
+
+echo "please enter the how many element s do you want" 
+read num 
+count=0
+declare -a my_array
+if [ $num -eq 0 ]; then 
+    echo "all the elements are added to the aarray" 
+else 
+    echo "please enter the $count element" 
+    read number 
+    my_array+=($number)
+    ((num--))
+    ((count++))
+
