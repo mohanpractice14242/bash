40bf97b7dbde942be26c79b384a5d81ee33e5552
mohan <mohan14242@gmail.com>
2024-08-03 20:11:14 +0530

    mohan

---
+++ arrays/6.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+
+
+my_array=("mohan" "chiru" "vijay")
+
+echo "do you want to delete all elements or only one element"
+read delete 
+if [ "$delete" -eq "all"]; then 
+    echo "deleteign the all elements" 
+    unset my_array 
+else 
+    echo "please enter the element index do you want to delete" 
+    read index
+    if [ "$index" -gt "${#my_array[#]}" ]; then 
+        echo "you have enterred the length greater than the array length"
+    else
+        echo "deleteign the $index element" 
+        unset $my_array[$index]
+    fi 
+fi
