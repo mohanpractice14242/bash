c6013d952138d15c571796fd1534d3ded2b51430
mohan <mohan14242@gmail.com>
2024-08-03 18:38:07 +0530

    mohan

--- arrays/1.sh
+++ arrays/1.sh
@@ -5,3 +5,7 @@ my_array=("mohan" "chiru" "vijay")
 #####accessign tthe varuables in the the array 
 
 echo "the first variable is ${my_array[0]}"
+
+
+###acessign the all arrays 
+echo "the all values in the array is ${my_array[@]}"
