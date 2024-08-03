f21807fb1f30920ebf3f06065f8f6b9442211822
mohan <mohan14242@gmail.com>
2024-08-03 19:56:16 +0530

    mohan

--- arrays/4.sh
+++ arrays/4.sh
@@ -2,8 +2,13 @@
 
 
 
-array=("mohan" "vijay" "chiru")
+array=("mohan" "vijay" "chiru" "nothing" "hofo")
 
 ###find the length the array 
 
 echo "the length of the array is ${#array[@]}"
+
+
+##slicing the array 
+
+echo "the slicing the array is ${array[@]:1:3}
