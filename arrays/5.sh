973d4352acda9ec7a1562f2ef1223fe8fc1e1765
mohan <mohan14242@gmail.com>
2024-08-03 20:01:55 +0530

    mohan

--- arrays/5.sh
+++ arrays/5.sh
@@ -2,10 +2,10 @@
 
 my_array=("mohan" "vijay" "chiru" "drtfyhuij" "dfghjnbv" "dyujbbhj")
 
-for element in "$my_array[@]"; do 
+for element in "${my_array[@]}"; do 
     echo "the elements are $element" 
 done 
 
-for i in "$my_array[@]";do 
+for i in "${my_array[@]}";do 
     echo "the $i element is $my_array[$i]"
 done
