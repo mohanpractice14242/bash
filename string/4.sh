a46767ce556764a2608ab9f5c18362dded857d2a
mohan <mohan14242@gmail.com>
2024-08-04 09:29:29 +0530

    mohan

--- string/4.sh
+++ string/4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 
-my_string= "thisf sdfshnabout the whole workdsftyugfvhjasf"
+my_string="thisf sdfshnabout the whole workdsftyugfvhjasf"
 
 substring=${my_string:5:10}
 echo "$substring"
