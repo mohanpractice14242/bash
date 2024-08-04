f7e1e3626c3bc0ec41704b9792a8618160a0d2fe
mohan <mohan14242@gmail.com>
2024-08-04 09:35:51 +0530

    mohan

---
+++ string/7.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+
+my_string="hello world this is the mohan mohan" 
+
+if [[$my_string == *"world"* ]]; then 
+    echo "the string containes the desired word" 
+else 
+    echo "the string does not contain the world" 
+fi
