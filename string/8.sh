90adc0ee5972a387147e2ca6d1f811de23b9941b
mohan <mohan14242@gmail.com>
2024-08-04 09:49:00 +0530

    mohan

--- string/8.sh
+++ string/8.sh
@@ -2,6 +2,6 @@
 
 
 my_array="  this is the mohan   " 
-trimmed=$*(echo $my_string | xargs)
+trimmed=$(echo $my_string | xargs)
 echo "original string $my_string" 
 echo "trimmed string is $trimmed"
