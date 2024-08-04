92053ee50ff5c312b67cb2c41d48b3df624ff3fb
mohan <mohan14242@gmail.com>
2024-08-04 10:10:16 +0530

    mohan

---
+++ integers/1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+
+###this is about the numbers 
+num=42 
+echo "the number variable is $num" 
+
+a=10 
+b=20 
+sum=$((a+b))
+echo "sum is $sum" 
+difference=$((a-b))
+echo "difference is $difference" 
