acdb1965388921f5ddd966cebcf751f1b8af77fe
mohan <mohan14242@gmail.com>
2024-08-06 17:38:07 +0530

    mohan

---
+++ check_conditions/7.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+##check the file is writable and its exist s
+
+if [ -x "1.sh" ]; then 
+    echo "the file is exist and is executable" 
+else 
+    echo "the file is not exists or not readable" 
+if 
