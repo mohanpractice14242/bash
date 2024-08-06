beffe85001f9cd5b676841c38f251ed65b7ba51b
mohan <mohan14242@gmail.com>
2024-08-06 17:12:34 +0530

    mohan

---
+++ string/14.sh
@@ -0,0 +1,12 @@
+#!/bin/bahs 
+
+echo "please enter teh strign 1" 
+read n1 
+echo "plaease ente rthe rstring2" 
+read n2 
+
+if [ $n1 != $n2 ]; then 
+    echo "both are not equal " 
+else 
+    echo "both are having the equal" 
+fi
