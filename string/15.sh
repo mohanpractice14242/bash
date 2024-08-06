855485bb333bfa9d516581aa40af4739e839aab9
mohan <mohan14242@gmail.com>
2024-08-06 17:14:47 +0530

    mohan

---
+++ string/15.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+
+echo "please enter the strin g with some length" 
+read n1 
+
+if [ -z $n1 ]; then 
+    echo "the string is zero length" 
+else 
+    echo "the strin gis not the ero length" 
+fi
