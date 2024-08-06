963627a2d1d84321b3177d1124639cec7161bfe2
mohan <mohan14242@gmail.com>
2024-08-06 18:50:49 +0530

    mohan

---
+++ conditional_statements/2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+read score
+
+if [ $score -ge 90 ]; then
+       echo "You got an A."
+   elif [ $score -ge 80 ]; then
+       echo "You got a B."
+   else
+       echo "You got a grade below B."
+   fi
