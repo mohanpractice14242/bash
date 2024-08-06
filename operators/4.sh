6334ab90942a1d0941f7d9d4b263087300c0b5cb
mohan <mohan14242@gmail.com>
2024-08-06 16:56:25 +0530

    mohan

---
+++ operators/4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+echo "please enter the n1 " 
+read n1 
+echo "please enter the n2 " 
+read n2 
+
+if [ $n1 -le $n2 ]; then 
+    echo  " n1 is equal toe n1" 
+else 
+    echo "n2 is greather than n2 " 
+fi 
