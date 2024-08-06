f246f8ddb4f09c690d5326715000d2654b204309
mohan <mohan14242@gmail.com>
2024-08-06 16:49:56 +0530

    mohan

---
+++ operators/1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+echo "pelase enter the number" 
+read n1 
+echo "pleae enter the number 2 " 
+read n2 
+
+if [ $n1 -eq $n2 ]; then 
+    echo "both are equal" 
+else 
+    echo "both are not equal" 
+    
