5a63edbf94f597638f359e4e9eeba1fe0203cc3f
mohan <mohan14242@gmail.com>
2024-08-06 17:08:37 +0530

    mohan

---
+++ operators/9.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+x=18 
+y=100000 
+
+if [ $x -ge 18 -o $y > 50000 ]; then 
+    echo "you are elgiible to go to pakistan" 
+else 
+    echo "you are not elgiibtle to go to pakistan" 
+fi
