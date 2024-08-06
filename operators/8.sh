b0c12d8776bdeecf72c6aadf71dbed83867f986a
mohan <mohan14242@gmail.com>
2024-08-06 17:06:26 +0530

    mohan

---
+++ operators/8.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+
+x= 18 
+y= 100000 
+
+if [ $x -ge 18 -a $y -gt 50000 ]; then 
+    echo "you are eligible for america" 
+else 
+    echo "you are not eligible for got to america" 
+fi
