aecae1f97e6b86522aca75305e47b4b28f87fea7
mohan <mohan14242@gmail.com>
2024-08-09 09:48:47 +0530

    mohan

--- whileloop/4.sh
+++ whileloop/4.sh
@@ -7,4 +7,7 @@ while [ "$status" != "complete" ]
 do 
     echo "waiting for process to complete"
     read -p " please enter the processs of your work" status 
+    if [ "$status" = "complete"]; the 
+        echo "process is completed" 
+    fi
 done
